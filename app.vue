1fd3f1e18e935a5560e8df287762efeae01a52fc
imLeonam <leonammrib@gmail.com>
2024-11-10 01:35:42 -0300

    feat(app): moved data

--- app.vue
+++ app.vue
@@ -1,80 +1,25 @@
 <script setup>
+import { data } from './server/data';
+
 const $toast = useToast();
 
-const normasLaudoTecnico = [
-  {
-    title: 'ABNT NBR 14724',
-    resume: 'Norma para a apresentação de trabalhos acadêmicos.',
-    description: 'A NBR 14724 estabelece os requisitos para formatação e apresentação de trabalhos acadêmicos, como monografias, dissertações e teses, orientando sobre aspectos como capa, sumário, referências, entre outros.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 5413',
-    resume: 'Iluminância de ambientes internos.',
-    description: 'A norma NBR 5413 define os valores de iluminância recomendados para diferentes tipos de ambientes internos, como escritórios e escolas, visando ao conforto visual e à eficiência no consumo de energia.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 10152',
-    resume: 'Níveis de ruído em ambientes internos.',
-    description: 'A NBR 10152 estabelece os níveis de ruído recomendados para diferentes ambientes, como salas de aula e escritórios, visando o conforto acústico e a saúde dos usuários.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 9050',
-    resume: 'Acessibilidade a edificações, mobiliário, espaços e equipamentos urbanos.',
-    description: 'Esta norma fornece diretrizes para promover a acessibilidade em edificações e espaços urbanos, garantindo a inclusão de pessoas com deficiência ou mobilidade reduzida.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 5410',
-    resume: 'Instalações elétricas de baixa tensão.',
-    description: 'A norma NBR 5410 trata das condições técnicas exigidas para garantir a segurança em instalações elétricas de baixa tensão, desde a concepção até a execução.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 6023',
-    resume: 'Referências bibliográficas.',
-    description: 'A norma NBR 6023 estabelece regras para elaboração de referências bibliográficas, padronizando a apresentação de informações de fontes utilizadas em trabalhos técnicos e acadêmicos.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 10520',
-    resume: 'Citações em documentos.',
-    description: 'A norma NBR 10520 orienta sobre a forma correta de fazer citações de autores em documentos acadêmicos, detalhando o uso de citações diretas e indiretas.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR ISO 9001',
-    resume: 'Sistema de gestão da qualidade.',
-    description: 'A norma ISO 9001 define os requisitos para um sistema de gestão da qualidade, focando na melhoria contínua dos processos e na satisfação do cliente.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR ISO 14001',
-    resume: 'Sistema de gestão ambiental.',
-    description: 'A ISO 14001 é uma norma internacional que fornece diretrizes para a implementação de um sistema de gestão ambiental, visando a redução de impactos ao meio ambiente.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  },
-  {
-    title: 'ABNT NBR 5991',
-    resume: 'Instalações de equipamentos médicos.',
-    description: 'A norma NBR 5991 apresenta requisitos para a instalação segura de equipamentos médicos em unidades de saúde, visando a segurança dos pacientes e dos profissionais de saúde.',
-    imgUrl: 'https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o='
-  }
-];
+const normasLaudoTecnico = ref([]);
+
+const fetchData = async () => {
+  normasLaudoTecnico.value = data;
+}
 
 const searchInput = ref('');
-const filteredNormasLaudoTecnico = ref([...normasLaudoTecnico]);
+const filteredNormasLaudoTecnico = ref([...normasLaudoTecnico.value]);
 
 const filterOptions = computed(() => {
   searchInput.value = searchInput.value.toString();
   if (searchInput.value === '') {
-    filteredNormasLaudoTecnico.value = normasLaudoTecnico;
+    filteredNormasLaudoTecnico.value = normasLaudoTecnico.value;
     return;
   }
 
-  filteredNormasLaudoTecnico.value = normasLaudoTecnico.filter((norma) =>
+  filteredNormasLaudoTecnico.value = normasLaudoTecnico.value.filter((norma) =>
     norma.title.includes(searchInput.value.toUpperCase())
   );
 });
@@ -113,6 +58,7 @@ const setEqualHeight = () => {
 };
 
 onMounted(() => {
+  fetchData();
   cardRefs.value = Array.from(document.querySelectorAll('.card-content'));
   setEqualHeight();
 });
@@ -138,8 +84,8 @@ const onDialogOpen = (item) => {
         icon="i-heroicons-magnifying-glass" @change="filterOptions" />
       <UContainer
         class="bg-slate-900 rounded-md mt-6 p-6 gap-5 overflow-y-auto overflow-x-hidden pt-7 flex flex-wrap justify-center lg:justify-between items-center transition-all w-full max-h-[86svh] h-fit">
-        <UCard v-for="(item, key) in filteredNormasLaudoTecnico" :key="key" :ref="cardRefs"
-          class="w-full md:w-96 sm:w-96 h-auto divide-none">
+        <UCard v-if="!!filteredNormasLaudoTecnico.length" v-for="(item, key) in filteredNormasLaudoTecnico" :key="key"
+          :ref="cardRefs" class="w-full md:w-96 sm:w-96 h-auto divide-none">
           <template #header>
             {{ item.title }}
           </template>
@@ -160,6 +106,10 @@ const onDialogOpen = (item) => {
             </UContainer>
           </template>
         </UCard>
+        <Ucard v-else
+          class="bg-slate-800 w-full text-center flex align-middle justify-center items-center rounded-lg h-10">
+          <span>Nenhuma norma encontrada</span>
+        </Ucard>
       </UContainer>
     </div>
 
@@ -184,12 +134,3 @@ const onDialogOpen = (item) => {
     </UModal>
   </div>
 </template>
-
-<style>
-/* .container-box {
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(10px);
-
-  scrollbar-width: thin;
-} */
-</style>
