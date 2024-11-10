103fd7df7ac7fddb24019b63bffe251039d709d7
imLeonam <leonammrib@gmail.com>
2024-11-10 01:35:27 -0300

    feat(server): added data

---
+++ server/data.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    title: "ABNT NBR 14724",
+    resume: "Norma para a apresentação de trabalhos acadêmicos.",
+    description: "A NBR 14724 estabelece os requisitos para formatação e apresentação de trabalhos acadêmicos, como monografias, dissertações e teses, orientando sobre aspectos como capa, sumário, referências, entre outros.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 5413",
+    resume: "Iluminância de ambientes internos.",
+    description: "A norma NBR 5413 define os valores de iluminância recomendados para diferentes tipos de ambientes internos, como escritórios e escolas, visando ao conforto visual e à eficiência no consumo de energia.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 10152",
+    resume: "Níveis de ruído em ambientes internos.",
+    description: "A NBR 10152 estabelece os níveis de ruído recomendados para diferentes ambientes, como salas de aula e escritórios, visando o conforto acústico e a saúde dos usuários.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 9050",
+    resume: "Acessibilidade a edificações, mobiliário, espaços e equipamentos urbanos.",
+    description: "Esta norma fornece diretrizes para promover a acessibilidade em edificações e espaços urbanos, garantindo a inclusão de pessoas com deficiência ou mobilidade reduzida.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 5410",
+    resume: "Instalações elétricas de baixa tensão.",
+    description: "A norma NBR 5410 trata das condições técnicas exigidas para garantir a segurança em instalações elétricas de baixa tensão, desde a concepção até a execução.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 6023",
+    resume: "Referências bibliográficas.",
+    description: "A norma NBR 6023 estabelece regras para elaboração de referências bibliográficas, padronizando a apresentação de informações de fontes utilizadas em trabalhos técnicos e acadêmicos.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 10520",
+    resume: "Citações em documentos.",
+    description: "A norma NBR 10520 orienta sobre a forma correta de fazer citações de autores em documentos acadêmicos, detalhando o uso de citações diretas e indiretas.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR ISO 9001",
+    resume: "Sistema de gestão da qualidade.",
+    description: "A norma ISO 9001 define os requisitos para um sistema de gestão da qualidade, focando na melhoria contínua dos processos e na satisfação do cliente.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR ISO 14001",
+    resume: "Sistema de gestão ambiental.",
+    description: "A ISO 14001 é uma norma internacional que fornece diretrizes para a implementação de um sistema de gestão ambiental, visando a redução de impactos ao meio ambiente.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  },
+  {
+    title: "ABNT NBR 5991",
+    resume: "Instalações de equipamentos médicos.",
+    description: "A norma NBR 5991 apresenta requisitos para a instalação segura de equipamentos médicos em unidades de saúde, visando a segurança dos pacientes e dos profissionais de saúde.",
+    imgUrl: "https://media.istockphoto.com/id/472290125/pt/vetorial/gato-em-windowsill.jpg?s=612x612&w=0&k=20&c=B0_F90grQj7RCjNoFhqX8YEVoRu9bMKaBRXX5One82o="
+  }
+]
+
+export { data };
