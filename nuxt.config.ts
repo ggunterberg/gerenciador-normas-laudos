8ff6f5839822f6dd0667b6feaca3069121d05ad8
imLeonam <leonammrib@gmail.com>
2024-11-10 01:34:49 -0300

    feat(config): disabling ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  ssr: false,
+  router: {
+    options: {
+      hashMode: true
+    }
+  }
 })
