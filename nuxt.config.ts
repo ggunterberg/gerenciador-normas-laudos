7d53457cbbb0c83f635ac71025bc0a9372f6f759
rossi <gustavodevrr@gmail.com>
2024-11-14 18:12:41 -0300

    chore: Adds output folder from 'pnpm generate' command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   ssr: false,
+  app: {
+    baseURL: "/gerenciador-normas-laudos/.output/public",
+  },
   router: {
     options: {
       hashMode: true
